4052d35b1c9bff2e88de12fe8cab34c729bd94da
URK21CS2035-RUDALPH <109648432+URK21CS2035-RUDALPH@users.noreply.github.com>
2022-10-12 10:35:14 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,45 +1,39 @@
-  from typing import List
+from operator import le
+from typing import List
 
-def merge_sort(myList) -> None:
-  if len(myList) > 1:
-        mid = len(myList) // 2
-        left = myList[:mid]
-        right = myList[mid:]
+def merge_sort(data) -> None:
+  # Write code here
+  if len(data)>1:
+    mid = (len(data))//2
+    l_arr = data[:mid]
+    r_arr = data[mid:]
 
-        # Recursive call on each half
-        merge_sort(left)
-        merge_sort(right)
+    merge_sort(l_arr)
+    merge_sort(r_arr)
 
-        # Two iterators for traversing the two halves
-        i = 0
-        j = 0
-        
-        # Iterator for the main list
-        k = 0
-        
-        while i < len(left) and j < len(right):
-            if left[i] <= right[j]:
-              # The value from the left half has been used
-              myList[k] = left[i]
-              # Move the iterator forward
-              i += 1
-            else:
-                myList[k] = right[j]
-                j += 1
-            # Move to the next slot
-            k += 1
+    i = 0
+    j = 0 
+    k = 0
 
-        # For all the remaining values
-        while i < len(left):
-            myList[k] = left[i]
-            i += 1
-            k += 1
+    while i < len(l_arr) and j < len(r_arr):
+      if l_arr[i] < r_arr[j]:
+        data[k] = l_arr[i]
+        i+=1
+      else:
+        data[k] = r_arr[j]
+        j+=1
+      k+=1
+    
+    while i < len(l_arr):
+      data[k] = l_arr[i]
+      i+=1
+      k+=1
+    while j < len(r_arr):
+      data[k] = r_arr[j]
+      j+=1
+      k+=1
 
-        while j < len(right):
-            myList[k]=right[j]
-            j += 1
-            k += 1
-
+    return data
 
 # Do not change the following code
 input_data = input()
